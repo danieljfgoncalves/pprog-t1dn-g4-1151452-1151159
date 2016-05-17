1a1ae8f59db70751bf459295c75661b2157b7a2f
Daniel Gonçalves <1151452@isep.ipp.pt>
2016-05-17 01:04:51 +0100

    [Atualizado] Criei as classes Organizador, RepresentanteExpositor, Avaliacao, RegistoMecanismos, ListaOrganizadores e ListaAtribuicoes pertences ao package model.

---
+++ src/centroexposicoes/model/ListaOrganizadores.java
@@ -0,0 +1,14 @@
+/*
+ * Fornece as classes que contém o modelo logístico de um centro de exposições.
+ */
+package centroexposicoes.model;
+
+/**
+ *
+ * @author Daniel Gonçalves 1151452
+ * @author Ivo Ferro 1151159
+ */
+public class ListaOrganizadores 
+{
+    
+}
